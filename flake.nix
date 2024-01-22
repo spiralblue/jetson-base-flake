9c71b625649878465b3fb5c213fc3bdbd5397e52
arduano <leonid.shchurov@gmail.com>
2024-01-22 17:38:24 +1100

    lock to revision

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS base for jetson testing";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.05?rev=70bdadeb94ffc8806c0570eb5c2695ad29f0e421";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
