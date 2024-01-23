32897dfe29995d9fe4eb1337bef297783707c284
arduano <leonid.shchurov@gmail.com>
2024-01-24 10:16:04 +1100

    Fail tolerance on the SSD

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -42,6 +42,7 @@
     {
       device = "/dev/disk/by-label/data";
       fsType = "ext4";
+      options = [ "verbose" "nofail" "noatime" "x-systemd.device-timeout=10s" ];
     };
 
   swapDevices = [ ];
